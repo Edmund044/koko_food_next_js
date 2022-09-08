95e64ec886c182386597020ee6aaac652e4a3c6d
EDMUND <edmund@mq1.smartcook.com>
2022-09-08 09:28:37 +0300

    workflow 2

--- pages/edit-profile.js
+++ pages/edit-profile.js
@@ -10,157 +10,219 @@ import Typography from '@mui/material/Typography';
 import EmailIcon from '@mui/icons-material/Email';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ApartmentIcon from '@mui/icons-material/Apartment';
-const editProfile = () => {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
+import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
+import WorkIcon from '@mui/icons-material/Work';
+import axios from 'axios';
+const register = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    axios(
+      {
+        url:"http://edmund044.pythonanywhere.com/create-employee",
+        method: "POST",
+        headers: {
+          'Access-Control-Allow-Origin' : '*',
+          'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+          },
+        data:{
           email: data.get('email'),
-          password: data.get('password'),
-        });
-      };
-        return (
-          <Grid container component="main" sx={{ height: '100vh' }}>
-           
-          <CssBaseline />
-          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          first_name: data.get('firstname'),
+          last_name: data.get('lastname'),
+          department: data.get('department'),
+          employee_number: data.get('employee_number'),
+          designation: data.get('designation')
+        },
+        timeout: 10000
+      }
+    )
+      .then((results) => {
+        // setinitiatives(results.data)
+        console.log(results.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  };
+    return (
+      <Grid container component="main" sx={{ height: '100vh' }}>
+       
+      <CssBaseline />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+      <Typography
+               variant="h3"
+               fontWeight="fontWeightBold"
+               style={{ mt: 3,
+                    mb: 2 ,
+                    color: '#00E1FD'
+              }}
+               >
+          <strong> KOKO FOOD</strong>
+          </Typography>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar
+                alt="KOKO FOOD"
+                src="/static/images/koko_logo.png"
+                sx={{ width: 70, height: 70 }}
+          />
           <Typography
-                   component="h1" 
-                   variant="h5"
-                   fontWeight="fontWeightBold"
-                   style={{ mt: 3,
-                        mb: 2 ,
-                        color: '#00E1FD'
-                  }}
-                   >
-              <strong> KOKO FOOD</strong>
-              </Typography>
-            <Box
-              sx={{
-                my: 8,
-                mx: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
+               component="h1" 
+               variant="h5"
+               fontWeight="fontWeightBold"
+               >
+          Edit Profile
+          </Typography>
+          <Box component="form"  onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="employee_number"
+                label="Employee Number"
+                name="employee_number"
+                autoComplete="employee_number"
+                autoFocus
+                type="number"
+                variant="standard"
+                InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <FormatListNumberedIcon />
+                  </InputAdornment>
+                                )
+                          }}
+              />
+            
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              variant="standard"
+              InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <EmailIcon />
+                </InputAdornment>
+                              )
+                         }}
+            />
+             <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="designation"
+                label="Designation"
+                name="designation"
+                autoComplete="designation"
+                autoFocus
+                variant="standard"
+                InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <WorkIcon />
+                  </InputAdornment>
+                                )
+                          }}
+              />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="firstname"
+              label="First Name"
+              type="text"
+              id="firstname"
+              variant="standard"
+              InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircleIcon />
+                </InputAdornment>
+                              )
+                         }}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="lastname"
+              label="Last Name"
+              type="text"
+              id="lastname"
+              variant="standard"
+              InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircleIcon />
+                </InputAdornment>
+                              )
+                         }}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="department"
+              label="Department"
+              type="text"
+              id="department"
+              variant="standard"
+              InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <ApartmentIcon />
+                </InputAdornment>
+                              )
+                         }}
+            />
+            <br></br>
+            <br></br>
+             <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              fullWidth
+              style={{ mt: 5,
+                    mb: 2 ,
+                    borderRadius: 28, 
+                    backgroundColor: '#00E1FD',
+                    color: '#FFFFFF'
               }}
             >
-              <Avatar
-                    alt="KOKO FOOD"
-                    src="/static/images/koko_logo.png"
-                    sx={{ width: 70, height: 70 }}
-              />
-              <Typography
-                   component="h1" 
-                   variant="h5"
-                   fontWeight="fontWeightBold"
-                   >
               Edit Profile
-              </Typography>
-              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                  variant="standard"
-                  InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <EmailIcon />
-                    </InputAdornment>
-                                  )
-                             }}
-                />
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="First Name"
-                  label="First Name"
-                  type="text"
-                  id="firstname"
-                  variant="standard"
-                  InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <AccountCircleIcon />
-                    </InputAdornment>
-                                  )
-                             }}
-                />
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="Last Name"
-                  label="Last Name"
-                  type="text"
-                  id="lastname"
-                  variant="standard"
-                  InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <AccountCircleIcon />
-                    </InputAdornment>
-                                  )
-                             }}
-                />
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="  Department"
-                  label="Department"
-                  type="text"
-                  id="department"
-                  variant="standard"
-                  InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <ApartmentIcon />
-                    </InputAdornment>
-                                  )
-                             }}
-                />
-                <br></br>
-                <br></br>
-                 <Button
-                  type="submit"
-                  variant="contained"
-                  size="large"
-                  fullWidth
-                  style={{ mt: 5,
-                        mb: 2 ,
-                        borderRadius: 28, 
-                        backgroundColor: '#00E1FD',
-                        color: '#FFFFFF'
-                  }}
-                >
-                  Edit
-                </Button>
-                
-              </Box>
-            </Box>
-          </Grid>
-          <Grid
-            item
-            xs={false}
-            sm={4}
-            md={7}
-            sx={{
-              backgroundImage: 'url(/static/images/edit.png)',
-              backgroundRepeat: 'no-repeat',
-              backgroundColor: '#00E1FD',
-              backgroundSize: 'contain',
-              backgroundPosition: 'center',
-            }}
-          />
-        </Grid>
-        );
+            </Button>
+            
+          </Box>
+        </Box>
+      </Grid>
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: 'url(/static/images/register_side.png)',
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: '#00E1FD',
+          backgroundSize: 'contain',
+          backgroundPosition: 'center',
+        }}
+      />
+    </Grid>
+    );
 }
- 
-export default editProfile;
+
+export default register;
